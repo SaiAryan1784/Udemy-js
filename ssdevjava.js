cd6746f0ae710613fae3d0518fd2b666dc2f5073
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-05-27 01:42:53 +0530

    sssdev course start

---
+++ ssdevjava.js
@@ -0,0 +1,13 @@
+document.body.innerHTML = "hello"
+
+//2a
+
+let cost = 0
+console.log("1 sour for 10")
+cost+=10
+console.log("3 burgers for 8")
+cost+=(8*3)
+console.log("1 icecream for 5")
+cost+=5
+
+console.log(`total = ${cost}`)
