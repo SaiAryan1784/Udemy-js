f5f36686ceb35bf39052b0510f12f018d4deefe6
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-06-01 02:12:43 +0530

    new learning

---
+++ ssdevjs2.js
@@ -0,0 +1,12 @@
+const product = {
+    Name : 'shirt',
+    Price : '$28',
+    ['colour'] : 'Red'
+}
+
+console.log(product)
+console.log(product.name)
+
+// console.log(product.colour) //error
+// console.log(product['colour']) //undefined if not created 
+
